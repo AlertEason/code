4811b6415f35c207f039f80ef81fff4caa3d879e
AlertEason <13916542+alerteason@user.noreply.gitee.com>
2025-04-19 02:45:40 +0800

    modified:   oj.c

--- oj.c
+++ oj.c
@@ -1,20 +1,29 @@
+#include <stdio.h>
 int main()
-{ int a[100],n,i,j,m,flag;
+{ int a[20][20],n,i,j,flag,col;
 scanf("%d",&n);
 for(i=0;i<n;i++)
-scanf("%d",&a[i]);
-for(i=1;i<n;i++)
-{ m=a[i];
-flag=0; 
-for(j=i-1;j>=0 && a[j]>m;j--) 
-{
-    a[j+1]=a[j];
-    flag=1;
-}
-if(flag) 
-a[j+1]=m;
-}
+for(j=0;j<n;j++)
+scanf("%d",&a[i][j]);
 for(i=0;i<n;i++)
-printf("%5d",a[i]);
+{ col=0;
+for(j=1;j<n;j++) 
+if(a[i][col]<a[i][j])
+col=j;
+flag=1; 
+for(j=0;j<n&&flag;j++)
+{
+    if(a[j][col]<a[i][col])
+    {
+        flag=0;
+    }
+}
+if(flag)
+{ printf("[%d,%d]=%d\n",i,col,a[i][col]);
+break;
+}
+}
+if(flag==0)
+printf("no");
 return 0;
 }
