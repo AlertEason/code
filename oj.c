c2d5498ec6f00c55d0f81831fd65242e95b19e46
AlertEason <13916542+alerteason@user.noreply.gitee.com>
2025-04-18 10:21:45 +0800

    modified:   oj.c

--- oj.c
+++ oj.c
@@ -5,10 +5,13 @@ for(i=0;i<n;i++)
 scanf("%d",&a[i]);
 for(i=1;i<n;i++)
 { m=a[i];
-flag=0; //先假定不需要插入
-for(j=i-1;j>=0 && a[j]>m;j--) //大元素往后移 ，腾出插入的位置
-【1】//请在此填入多行代码
-if(flag) //若需要插入
+flag=0; 
+for(j=i-1;j>=0 && a[j]>m;j--) 
+{
+    a[j+1]=a[j];
+    flag=1;
+}
+if(flag) 
 a[j+1]=m;
 }
 for(i=0;i<n;i++)
