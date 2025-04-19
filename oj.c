b94665f43575042a68fea3f93b1407a1b0e02bbb
AlertEason <13916542+alerteason@user.noreply.gitee.com>
2025-04-19 15:53:51 +0800

    modified:   oj.c

--- oj.c
+++ oj.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
 int main()
-{ int a[20][20],n,i,j,flag,col;
-scanf("%d",&n);
-for(i=0;i<n;i++)
-for(j=0;j<n;j++)
-scanf("%d",&a[i][j]);
-for(i=0;i<n;i++)
-{ col=0;
-for(j=1;j<n;j++) 
-if(a[i][col]<a[i][j])
-col=j;
-flag=1; 
-for(j=0;j<n&&flag;j++)
 {
-    if(a[j][col]<a[i][col])
+    int a[20][20],max[20],n,m,k,i,j,flag;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",&a[i][j]);
+    max[0]=a[0][0];
+    for(i=1;i<n;i++)
     {
-        flag=0;
+        m=max[i]=a[0][i];
+        flag=0; 
+        for(j=i-1;j>=0 && max[j]<m;j--) 
+        {
+        max[j+1]=max[j];
+        flag=1; 
+        }
+        if(flag) 
+        max[j+1]=m;
     }
-}
-if(flag)
-{ printf("[%d,%d]=%d\n",i,col,a[i][col]);
-break;
-}
-}
-if(flag==0)
-printf("no");
-return 0;
+    for(i=1;i<n;i++) 
+        for(j=0;j<n;j++)
+        { m=a[i][j];
+        flag=0;
+        for(k=n-1;k>=0 && m>max[k];k--)
+        {
+            max[k+1]=max[k];
+            flag=1;
+        }
+        if(flag)
+        max[k+1]=m;
+        }
+    for(i=0;i<n;i++)
+    printf("%5d",max[i]);
+    return 0;
 }
