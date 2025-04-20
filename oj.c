abc30d734dd9ca3b455b258fc34f1d3acd7450c0
AlertEason <13916542+alerteason@user.noreply.gitee.com>
2025-04-20 21:33:57 +0800

    modified:   oj.c

--- oj.c
+++ oj.c
@@ -1,37 +1,30 @@
 #include <stdio.h>
-int main()
-{
-    int a[20][20],max[20],n,m,k,i,j,flag;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-        for(j=0;j<n;j++)
-            scanf("%d",&a[i][j]);
-    max[0]=a[0][0];
-    for(i=1;i<n;i++)
-    {
-        m=max[i]=a[0][i];
-        flag=0; 
-        for(j=i-1;j>=0 && max[j]<m;j--) 
-        {
-        max[j+1]=max[j];
-        flag=1; 
+#include<string.h>
+
+int main() {
+    int n, m;
+    scanf("%d,%d", &n, &m);
+    int a[n];
+    memset(a, 0, sizeof(a));
+    int count = n, counting = 0, i = 0;
+    while(count > m){
+        if(a[i] == 0)counting++;
+        if(counting == m){
+            a[i] = 1;
+            counting = 0;
+            count--;
         }
-        if(flag) 
-        max[j+1]=m;
+        i++;
+        if (i > n) i = 0;
+
+        
     }
-    for(i=1;i<n;i++) 
-        for(j=0;j<n;j++)
-        { m=a[i][j];
-        flag=0;
-        for(k=n-1;k>=0 && m>max[k];k--)
-        {
-            max[k+1]=max[k];
-            flag=1;
+
+    for(int i = 0; i < n; i++){
+        if(!a[i]){
+            printf("%5d ", i + 1);
         }
-        if(flag)
-        max[k+1]=m;
-        }
-    for(i=0;i<n;i++)
-    printf("%5d",max[i]);
-    return 0;
+    }
+
+    
 }
