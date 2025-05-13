be1db1ab09aa0b7baa258600d0accf42aebcfc80
AlertEason <13916542+alerteason@user.noreply.gitee.com>
2025-05-13 13:42:30 +0800

    2025-05-13 13:42:20

--- oj.c
+++ oj.c
@@ -1,28 +1,32 @@
-#include <stdio.h>
-#define N 100
-void insert(int s[],int x,int n)
-{ int i;
-if(x>s[n-1])
-s[n]=x;
-else
-{ for(i=n;i>0&&s[i-1]>x;i--)
-s[i]=s[i-1];
-s[i]=x;
-}
-}
-void insertsort(int s[],int n)
+#include "stdio.h"
+#include "string.h"
+int findsubstring(char *s,char *sub)
 {
-    for(int i = 1; i < n; i++) insert(s, s[i], i);
-    
+    int len_s = strlen(s);
+    int len_sub = strlen(sub);
+    for(int i = 0; i <= len_s - len_sub; i++)
+    {
+        int j;
+        for(j = 0; j < len_sub; j++)
+        {
+            if(s[i+j] != sub[j])
+                break;
+        }
+        if(j == len_sub)
+            return i;
+    }
+    return -1;
 }
+
 int main()
-{
-int i,n,a[N];
-scanf("%d",&n);
-for(i=0;i<n;i++)
-scanf("%d",&a[i]);
-insertsort(a,n);
-for(i=0;i<n;i++)
-printf("%5d",a[i]);
+{ char line[80],substr[10];
+int index;
+gets(line);
+gets(substr);
+index=findsubstring(line,substr);
+if(index>=0)
+printf("yes",index);
+else
+printf("no");
 return 0;
 }
