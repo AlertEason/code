312d80d71acbb0fbbdf4a061415399fe28be22ce
AlertEason <13916542+alerteason@user.noreply.gitee.com>
2025-05-15 21:30:40 +0800

    2025-05-15 21:30:36

--- oj.c
+++ oj.c
@@ -1,67 +1,39 @@
 #include <stdio.h>
 #include <string.h>
-#define N 100
 
-int isalpha(char ch) //判断ch是否为字母，ch为小写字母返回1，大写字母返回-1，其他字符则返回0
-{
-    if(ch>='a'&&ch<='z')
-        return 1;
-    else if(ch>='A'&&ch<='Z')
-        return -1;
-    else
-        return 0;
-}
-
-int extract(char *s,char words[][80]) //从字符串s中提取单词，并存放于words中，返回提取的单词个数
-{
-    int i,j,n,flag;
-    char ch;
-    i=0,n=-1,flag=0;
-    while(ch=s[i])
-    {
-        if(isalpha(ch))
-        {
-            if(flag==0) //新单词开始
-            {
-                // 请在此处填写多行语句
-                // 提示：需要处理新单词的开始，包括更新标志、单词计数和字符索引
-                // 并根据字符大小写进行相应处理
+void unzip(char *s1, char *s2) {
+    char *p = s1, *q = s2;
+    int flag = 0, n;
+    while (*p) {
+        if (*p >= '0' && *p <= '9') {
+           if(!flag){
+            flag = 1;
+            n = *p - '0';
             }
-            else
-                words[n][++j]=ch;
+            else{
+            n = n * 10 + (*p - '0');
+            }
+            p++;            
+        }else if((*p >= 'A'&& *p <= 'Z')||(*p >= 'a' && *p <= 'z')){
+            if(flag){
+                for(int i = 0; i < n; i++){
+                    *q++ = *p;
+                }
+                flag = 0;
+            }else{
+                *q++ = *p;
+            }
+            p++;
+            
         }
-        else if(flag) //当前单词提取结束
-        {
-            words[n][j]='\0';
-            flag=0;
-        }
-        i++;
     }
-    return n+1;
+    *q = '\0'; // 添加字符串结束符
 }
 
-void strsort(char str[][80],int n) //采用冒泡算法对str中的n个字符串按升序排序
-{
-    int i,j;
-    char t[80];
-    for(i=1;i<n;i++)
-        for(j=0;j<n-i;j++)
-            if(strcmp(str[j],str[j+1])>0)
-            {
-                strcpy(t,str[j]);
-                strcpy(str[j], str[j+1]);
-                strcpy(str[j+1],t);
-            }
-}
-
-int main()
-{
-    char line[81],words[N][80];
-    int i,n;
-    gets(line);
-    n=extract(line,words);//提取单词
-    strsort(words,n); //单词排序
-    for(i=0;i<n;i++)
-        puts(words[i]);
+int main() {
+    char s1[80], s2[1000];
+    gets(s1); // 注意：gets存在缓冲区溢出风险，建议使用fgets
+    unzip(s1, s2);
+    puts(s2);
     return 0;
 }
