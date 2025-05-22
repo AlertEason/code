870a12c2b07363dd9c0b09e4a7f7f5d337ddf52f
AlertEason <13916542+alerteason@user.noreply.gitee.com>
2025-05-22 20:17:50 +0800

    SY1101

--- oj.c
+++ oj.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
-double power(double x,int n)
+double power(double x, int n)
 {
-    if(n==0) return 1;
-    if(n>0) return x*power(x,n-1);
-    else return 1/power(x,-n);
+    if (n == 0)
+        return 1;
+    if (n > 0)
+        return x * power(x, n - 1);
+    else
+        return 1 / power(x, -n);
 }
 int main()
-{ double x,a;
-int n;
-scanf("%lf,%d",&x,&n);
-a=power(x,n);
-printf("%g",a);
-return 0;
+{
+    double x, a;
+    int n;
+    scanf("%lf,%d", &x, &n);
+    a = power(x, n);
+    printf("%g", a);
+    return 0;
 }
