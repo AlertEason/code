c7c21ba2e62e5f852e5142dfc9799812db10afc7
AlertEason <13916542+alerteason@user.noreply.gitee.com>
2025-05-22 13:43:41 +0800

    2025-05-22 13:43:32

--- oj.c
+++ oj.c
@@ -1,45 +1,26 @@
-#include"stdio.h"
-#include"stdlib.h"
-#define N 10
-void input(int (*s)[N],int n);
-int *linemax(int (*s)[N],int n);
+#include <stdio.h>
+#include <stdlib.h>
 
+struct Time
+{ int hour;
+int minute;
+int second;
+};
 int main()
-{
-    int i,n,a[N][N],*p; // 修正：p应为指针类型
-    scanf("%d",&n);
-    input(a,n);
-    p=linemax(a,n);
-    for(i=0;i<n;i++)
-        printf("%5d",*(p+i)); // 修正：正确解引用指针访问元素
-    free(p); // 修正：删除多余符号
-    return 0;
-}
-
-void input(int (*s)[N],int n)
-{
-    int i,j,*p;
-    for(i=0;i<n;i++,s++)
-        for(p=*s,j=0;j<n;j++,p++)
-            scanf("%d",p);
-}
-
-int *linemax(int (*s)[N],int n)
-{
-    int *p,*r,i,j,max;
-    if((r=(int *)calloc(n,sizeof(int)))!=NULL)//动态分配内存构建存放最大值的数组
-    {
-        for(i=0;i<n;i++,s++)
-        {
-            max=**s;//将当前行的第一个元素的值赋给变量 max
-            for(j=1;j<n;j++){
-                if(*(*s+j)>max)
-                    max=*(*s+j);
-            }
-            *(r+i)=max;
-        }
-        return r;
-    }
-    else
-        return NULL;
+{ int n;
+struct Time t1;
+scanf("%d:%d:%d", &t1.hour, &t1.minute, &t1.second);
+scanf("%d", &n);
+t1.hour += n / 3600;
+n = n % 3600;
+t1.minute += n / 60;
+n = n % 60;
+t1.second += n;
+t1.minute += t1.second / 60;
+t1.second = t1.second % 60;
+t1.hour += t1.minute / 60;
+t1.minute = t1.minute % 60;
+t1.hour = t1.hour % 24;
+printf("%02d:%02d:%02d", t1.hour, t1.minute, t1.second);
+return 0;
 }
