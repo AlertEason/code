c09144eadf666ce72cc6fe430b7239776d719f2c
AlertEason <13916542+alerteason@user.noreply.gitee.com>
2025-05-24 15:01:52 +0800

    ZY0801非正规解法

--- oj.c
+++ oj.c
@@ -1,22 +1,9 @@
 #include <stdio.h>
-#include <ctype.h>
-#include <string.h>
-#include <stdlib.h>
 
-int main() {
-    char str[100];
-    char hex_str[100] = "";
-    int i, j = 0;
-    fgets(str, sizeof(str), stdin);
-    for (i = 0; str[i] != '\0'; i++) {
-        if (str[i] == '-' && (i == 0 || (str[i - 1] != '-' &&!isxdigit(str[i - 1])))) {
-            hex_str[j++] = str[i];
-        } else if (isxdigit(str[i])) {
-            hex_str[j++] = str[i];
-        }
-    }
-    hex_str[j] = '\0';
-    long decimal = strtol(hex_str, NULL, 16);
-    printf("%s:%ld", hex_str, decimal);
-    return 0;
+int main(void)
+{
+    double n;
+    scanf("%lf",&n);
+    printf("%d\n", (int)n);
+    printf("%g\n", n - (int)n);
 }
