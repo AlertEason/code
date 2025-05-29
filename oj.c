37ac97640e6e8512af9416d17dd4acdb837b907a
AlertEason <13916542+alerteason@user.noreply.gitee.com>
2025-05-29 19:28:09 +0800

    contest1002

--- oj.c
+++ oj.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 #include <string.h>
-#define MAX 1000
 
 int main(void)
 {
-    char str[MAX] = "";
+    char str[1000] = "";
     fgets(str, sizeof(str), stdin);
-    char n;
-    scanf("%c", &n);
     int i = 0;
+    str[strlen(str) - 1] = '\0'; 
+    int result[4] = {0}; 
     while(1){
         if(str[i] == '\0'){
             break;
-        }
-        if (str[i] != n){
-            printf("%c", str[i]);
+        }else if(str[i] >= 'A' && str[i] <= 'Z'){
+            result[0]++;
+        }else if(str[i] >= 'a' && str[i] <= 'z'){
+            result[1]++;
+        }else if(str[i] >= '0' && str[i] <= '9'){
+            result[2]++;
+        }else{
+            result[3]++;
         }
         i++;
-        
     }
+    for (int i = 0; i < 3; i++){
+        printf("%d,", result[i]);
+    }
+    printf("%d", result[3]);
     return 0;
-}  
+}
