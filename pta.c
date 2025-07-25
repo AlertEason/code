78deb0725e1466c2f589f6b2f8ad4651f09ff4b8
AlertEason <easonwork2024@outlook.com>
2025-07-25 21:15:48 +0800

    modified:   pta.c

--- pta.c
+++ pta.c
@@ -1,39 +1,37 @@
 #include <stdio.h>
 #define MAXN 20
 
-void CountOff( int n, int m, int out[] );
+void strmcpy( char *t, int m, char *s );
+void ReadString( char s[] ); /* 由裁判实现，略去不表 */
 
 int main()
 {
-    int out[MAXN], n, m;
-    int i;
+    char t[MAXN], s[MAXN];
+    int m;
     
-    scanf("%d %d", &n, &m);
-    CountOff( n, m, out );   
-    for ( i = 0; i < n; i++ )
-        printf("%d ", out[i]);
-    printf("\n");
-    
+    scanf("%d\n", &m);
+    ReadString(t);
+    strmcpy( t, m, s );
+    printf("%s\n", s);
+
     return 0;
 }
 
 /* 你的代码将被嵌在这里 */
-
-void CountOff(int n, int m, int out[]) {
-    int check[MAXN] = {0};  // 标记数组，0表示未出列，1表示已出列
-    int count = 0;          // 当前报数
-    int index = 0;          // 当前位置
-    int step = 0;           // 出列顺序
-    
-    while (step < n) {
-        if (!check[index]) {
-            count++;
-            if (count == m) {
-                out[index] = ++step;  // 记录出列顺序
-                count = 0;            // 重置报数
-                check[index] = 1;     // 标记为已出列
-            }
-        }
-        index = (index + 1) % n;      // 循环遍历
+void strmcpy( char *t, int m, char *s )
+{
+    int len = 0;
+    while(t[len] != '\0') len++;
+    if(len < m)
+    {
+        *s = '\0';
+        return;
     }
+    int i = m - 1;
+    while(i < len)
+    {
+        s[i - m + 1] = t[i];
+        i++;
+    }
+    s[i - m + 1] = '\0';
 }
